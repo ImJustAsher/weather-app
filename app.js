96e504b870d5ee67c62492225d5d08dbaec71824
Asher <asher_3@hotmail.co.uk>
2020-02-18 16:53:27 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+	let long;
+	let lat;
+	let temperatureDescription = document.querySelector('.temperature-description');
+	let temperatureDegree = document.querySelector('.temperature-degree');
+	let locationTimezone = document.querySelector('.location-timezone');
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+
+			const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+			const api = `${proxy}
+            https://api.darksky.net/forecast/6ec30dfc1925a3d98ba401ff0289bee4/${lat}, ${long}`;
+			fetch(api)
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					console.log(data);
+					const { temperature, summary } = data.currently;
+					// set DOM elements from the API
+					temperatureDegree.textContent = temperature;
+					temperatureDescription.textContent = summary;
+					locationTimezone.textContent = data.timezone;
+				});
+		});
+	}
+});
